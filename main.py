427a2a3824c189e9633e81b0b0a661acb3e713bc
Witek <witoldnocon@gmail.com>
2020-12-09 11:13:18 +0100

    Quadratic equation solving (roots)

--- main.py
+++ main.py
@@ -3,11 +3,45 @@
 from tkinter import *
 from tkinter import messagebox
 
+from project_mathematics import quadratic_eqiation
+
+
 def quit_program():
     answer = messagebox.askyesno("Podejmij decyzję", "Czy zakończyć program?")
     if answer:
         root.destroy()
 
+def get_a_b_c():
+    a = a_entry.get()
+    b = b_entry.get()
+    c = c_entry.get()
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        # print('a = ', a, 'b = ', b, 'c = ', c)
+    except ValueError:
+        messagebox.showerror("Błąd",
+                             "Nie możba konwertować wpisanych wartości do liczb. Popraw proszę swoje zachowanie")
+        return None
+    return [a, b, c]
+
+def calcul():
+    result = get_a_b_c()
+    if result is not None:
+        a = result[0]
+        b = result[1]
+        c = result[2]
+        result = quadratic_eqiation(a, b, c)
+        if len(result) == 0:
+            s = "Brak rozwiązań"
+        elif len(result) == 1:
+            s = "x0 = %+10.3f" % result[0]
+        else:
+            s = "x01 = %+10.3f    x02 = %+10.3f" % (result[0], result[1])
+        result_lbl.config(text=s)
+    else:
+        result_lbl.config(text='---')
 
 root = Tk()
 root.title('Rozwiązywanie równania kwadratowego')
@@ -19,6 +53,9 @@ toolbar.pack(side=TOP, fill=BOTH)
 # przycisk, który będzie zamykać program
 stop_btn = Button(toolbar, text="Stop", fg='red', command=quit_program)
 stop_btn.pack(side=LEFT)
+
+calcul_btn = Button(toolbar, text="Rozwiąż równanie", command=calcul)
+calcul_btn.pack(side=LEFT)
 
 # a, b, c Entry
 leftbar = Frame(root)
@@ -38,5 +75,9 @@ Label(leftbar, text="c").grid(row=2, column=0)
 c_entry = Entry(leftbar, width=10)
 c_entry.insert(0, str(-2.0))
 c_entry.grid(row=2, column=1)
+
+# tekst wyniku
+result_lbl = Label(root, text="Jeszcze nie policzono pierwiastków...")
+result_lbl.pack(side=TOP)
 
 mainloop()
---
+++ project_mathematics.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+
+def quadratic_eqiation(a, b, c):
+    delta = b*b-4*a*c
+    if delta < 0:
+        return []
+    elif delta == 0:
+        return [-b*(2*a)]
+    else:
+        x1 = (-b-sqrt(delta))/(2*a)
+        x2 = (-b+sqrt(delta))/(2*a)
+        return [x1, x2]
+
+
+# def compute_function(a, b, c, xmin, xmax, dx):
+#     xx = []
+#     yy = []
+#     n = int((xmax - xmin)/dx + 1)
+#     for i in range(n):
+#         x = xmin + i*dx
+#         xx.append(x)
+#
+#         y = a*x*x + b*x + c
+#         yy.append(y)
+#
+#     # zwrócenie wyników (duże X i Y)S
+#     return xx, yy
